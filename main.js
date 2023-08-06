548c9414ef03f40c642cd7775189d453bd7bb20b
sina <sinarahbari@yahoo.com>
2023-08-06 14:48:23 +0330

    swiper TOP commited

---
+++ main.js
@@ -0,0 +1,18 @@
+// swiper Top start
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//   swiper Top end
