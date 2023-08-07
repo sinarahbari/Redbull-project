ad9cbc720e2fc70a014f1527a4034549d9092244
sina <sinarahbari@yahoo.com>
2023-08-07 15:15:18 +0330

    gallery commited

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // swiper Top start
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".SwiperTop", {
   spaceBetween: 0,
   centeredSlides: true,
   autoplay: {
@@ -16,3 +16,20 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 //   swiper Top end
+
+// swiper bottom start
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// swiper bottom end
